05177e74ce062fb002aefb596e4cfaaed19d61d1
PatricNox <eronoxsmail2@gmail.com>
2018-09-07 23:18:44 +0200

    Update `includes.js` file structure

--- layout/includes.js
+++ layout/includes.js
@@ -7,21 +7,21 @@
 
  /** Input Field  **/
 
-// Expand input width as user types
-let $i = 2;
-document.addEventListener('input', function (e) {
-    $i++;
-    event.target.style.width = $i + '%';
-}, false);
+    // Expand input width as user types
+    let $i = 2;
+    document.addEventListener('input', function (e) {
+        $i++;
+        event.target.style.width = $i + '%';
+    }, false);
 
-// Reset input fields width on Enter
-// This a workaround for the eventual massive 
-// and endless potentional width it can have after
-// a longer usage of the field.
-document.addEventListener('keyDown', function (e) {
-    const input = document.querySelector('#cmd');
-    input.style.width = "auto";
-    $i = 2;
-}, false)
+    // Reset input fields width on Enter
+    // This a workaround for the eventual massive 
+    // and endless potentional width it can have after
+    // a longer usage of the field.
+    document.addEventListener('keyDown', function (e) {
+        const input = document.querySelector('#cmd');
+        input.style.width = "auto";
+        $i = 2;
+    }, false)
 
  /** End of Input Field **/
